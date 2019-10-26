74b2c34140cba2a9093cac05e915f87a27e89509
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2019-10-26 18:16:36 -0300

    create server

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-console.log("Hello Wold");
+//var express  = require('express');
+//var app = express();
 
 
+const http = require('http');
+
+var conec = function (req, res){
+    res.end('Hello World!');
+}
+
+http.createServer(conec);
+
+http.listen(3000);
+
+
+
+
+
+/*
+var hostname = 'localhost';
+var port = 8080;
+
+http.createServer(function(req, res){
+    res.end("<h1>Hello World</h1>   ");
+}).listen(port);
+
+link = `http://${hostname}:${port}/`;
+
+console.log(`Server running at `+ link);
+*/
