122fd8ab5b9c9e081fefffaaef68e9694e73411a
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2019-10-28 15:08:16 -0300

    mudaças versao 2.0

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 var express  = require('express');
 var app = express();
+var Sequelize = require('sequilize');
 
 var hostname = "127.0.0.1";
 var port = 3000;
 var link = `http://${hostname}:${port}/`;
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize('database', 'username', 'password', {
+  host: 'localhost',
+  dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+});
+ 
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
 
 app.get('/',function (req, res){
     res.sendFile(__dirname + "/src/login.html");
@@ -13,3 +28,4 @@ app.get('/',function (req, res){
 app.listen(port, function(){
     console.log(`Server running at `+ link);
 });
+
