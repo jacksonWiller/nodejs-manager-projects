4b2f9d7c8b248cb6832a22f0dbd6c3d2c7434987
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2019-11-17 11:41:32 -0200

    midleware

--- app.js
+++ app.js
@@ -5,8 +5,25 @@
     const app = express();
     const admin = require("./routes/admin");
     const path = require("path");
-    // const mongoose = require('mongoose');
+    const mongoose = require('mongoose');
+    const session = require('express-session');
+    const flash = require('connect-flash');
 // Configurações 
+    //sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash());
+
+    //Middleware
+        app.use((req, res, next) => {
+            res.locals.nome = seccess_msg = req.flash("success_msg");
+            res.locals.error_msg = req.flash("error_msg");
+            next();
+        })
+
     //Body Parse
         app.use(bodyParse.urlencoded({extended: true}));
         app.use(bodyParse.json());
@@ -14,9 +31,15 @@
         app.engine('handlebars', handlebars({defaultLayout: 'main'}));
         app.set('view engine', 'handlebars');
     // Mongoose
-        //Em breve
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://localhost/blogapp").then(() => {
+            console.log("Conectado ao mongo");  
+        }).catch((err) => {
+            console.log("Erro ao se conectar:"+err)
+        })
     // Public
         app.use(express.static(path.join(__dirname,"public")));
+
 // Rotas
     app.get('/', (req, res) => {
         res.send("Página de post"); 
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Categoria");
+const Categoria = mongoose.model("categorias");
+
 
 router.get('/', (req, res) => {
    res.render("admin/index"); 
@@ -12,6 +16,46 @@ router.get('/post', (req, res) => {
  router.get('/categoria', (req, res) => {
     res.render("admin/categorias"); 
  })
+
+ router.get('/categoria/add', (req, res) => {
+   res.render("admin/addCategorias"); 
+ })
+
+ router.post('/categorias/nova', (req, res) => {
+   
+   var erros = [];
+   
+   if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+      erros.push({texto: "nome inválido"})
+   }
+   
+   if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+      erros.push({texto: "Slug invalido"}
+   )}
+   
+   if(req.body.nome.length < 2){
+      erros.push({texto: "Nome da categoria ´e muito pequeno"})
+   }
+
+   if(erros.length > 0){
+      res.render("admin/addCategorias", {erros: erros})
+   }else{
+      const novaCategoria = {
+         nome: req.body.nome,
+         slug: req.body.slug
+      }
+   
+      new Categoria(novaCategoria).save().then(() => {
+         console.log("Categiria salva com sucesso!");
+         res.redirect("/admin/categorias")
+      }).catch((err) => {
+         req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamentr ")
+         console.log("Erro ao salvar categoria!"+err);
+      })
+   }
+})
+
+
 
 
 
