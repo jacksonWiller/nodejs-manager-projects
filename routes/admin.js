90f00909a2dc64973a1220bb066de7da039ef61f
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2019-11-26 01:11:36 -0200

    CRUD categorias

--- routes/admin.js
+++ routes/admin.js
@@ -118,7 +118,26 @@ router.post('/categorias/edit',(req,res)=>{
       console.log(req.body.slug)
   })
 })
-
+/*
+router.post("/categorias/deletar", (req, res)=> {
+   Categoria.remove({_id: req.body.id}).then(() =>{
+      res.flash("sucess_msg", "Categoria deletada com sucesso!") 
+      res.redirect("/admin/categorias")
+   }).catch((err) => {
+      req.flesh("error_msg", "Houve um erro ao deletar a categoria")
+      res.redirect("/admin/categorias")
+   })
+})
+*/
+router.post('/categorias/deletar',(req, res)=>{
+   Categoria.deleteOne({_id: req.body.id}).then(()=>{
+     req.flash('success_msg', 'Categoria removida com sucesso!')
+     res.redirect('/admin/categorias')
+   }).catch((err)=>{
+     req.flash('error_msg', 'Houve um erro ao remover a categoria!')
+     res.redirect('/admin/categorias')
+   })
+ })
 
 router.post
 
