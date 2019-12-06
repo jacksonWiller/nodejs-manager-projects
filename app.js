666e865d009bb8f7bfc0eb74d36aa3a0e0b5a3a7
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2019-12-06 02:24:47 -0200

    model & form usuario

--- app.js
+++ app.js
@@ -10,6 +10,9 @@
     const flash = require('connect-flash');
     require("./models/Postagem")
     const Postagem = mongoose.model("postagens")
+    require('./models/Categoria')
+    const Categoria = mongoose.model('categorias')
+    const usuarios = require('./routes/usuario')
 
 // Configurações 
     //sessão
@@ -68,6 +71,35 @@
         })
       })
 
+      app.get('/categorias', (req,res)=>{
+        Categoria.find().then((categorias)=>{
+          res.render('categorias/index', {categorias:categorias})
+        }).catch((err)=>{
+          req.flash('error_msg','Houve um erro interno ao listar as categorias.')
+          res.redirect('/')
+        })
+      })
+
+      app.get('/categorias/:slug', (req,res)=>{
+        Categoria.findOne({slug:req.params.slug}).then((categoria)=>{
+          if(categoria){
+            Postagem.find({categoria:categoria._id}).then((postagens)=>{
+              res.render('categorias/postagens',{postagens:postagens,categoria:categoria})
+            }).catch((err)=>{
+              req.flash('error_msg', 'Houve um erro ao listar os posts!')
+              res.redirect('/')
+            })
+          }else {
+            req.flash('error_msg', 'Esta categoria não existe.')
+            res.redirect('/')
+          }
+        }).catch((err)=>{
+          req.flash('error_msg','Erro ao carregar a página desta categoria.')
+          res.redirect('/')
+        })
+      })
+
+
     app.get('/404', (req,res)=>{
         res.send('Error 404!')
     })
@@ -78,6 +110,7 @@
     })
 
     app.use('/admin', admin);
+    app.use('/usuarios', usuarios)
     
 //Outros
 const PORT = 8081
