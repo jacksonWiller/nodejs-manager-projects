d3aa832b2b48c9455b3033e48cff10fd8b1f509f
Jackson Willer Duarte <jacksonwillerduarte@gmail.com>
2019-12-10 02:50:40 -0200

    A

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 //if(process.env.Node_ENV=='production'){
   module.exports = {mongoURI:"mongodb+srv://jacksonw:jacksonw@cluster0-9a0eq.gcp.mongodb.net/test?retryWrites=true&w=majority"}
-  //  }else{
+   // }else{
   //module.exports = {mongoURI:'mongodb://localhost/blogapp'}
 //}
